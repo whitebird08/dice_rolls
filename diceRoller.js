55992323113e0d41753de901843241c1250d1c9e
davidyoakum <dyoakum@gmail.com>
2015-08-03 13:39:04 -0600

    some dice rollin practice

---
+++ diceRoller.js
@@ -0,0 +1,12 @@
+var diceRoller = function(numOfRolls, sideOfDie) {
+  var newArray = [];
+  if (sideOfDie == NaN) {
+    var sideOfDie = 6;
+  }
+  for (var i = 0; i < numOfRolls; i++) {
+  newArray.push(Math.floor(Math.random() * sideOfDie) + 1);
+  }
+  return newArray;
+};
+
+console.log(diceRoller(10));
